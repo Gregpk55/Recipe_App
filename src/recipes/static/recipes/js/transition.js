c1c6d0e6c20ce77f44b5c5d19bbd46e398bf1fee
Gregpk55 <gregk557@gmail.com>
2023-09-22 13:33:15 -0700

    2.6 transitions update

--- src/recipes/static/recipes/js/transition.js
+++ src/recipes/static/recipes/js/transition.js
@@ -1,23 +1,31 @@
+document.addEventListener('DOMContentLoaded', applyFadeInTransition);
+
+document.addEventListener('click', function(event) {
+    if (event.target.tagName === "A") {
+        applyFadeOutTransition(event, () => {
+            window.location.href = event.target.href;
+        });
+    }
+});
+
+document.addEventListener('submit', function(event) {
+    if (event.target.tagName === "FORM") {
+        applyFadeOutTransition(event, () => {
+            event.target.submit();
+        });
+    }
+});
+
 function applyFadeInTransition() {
     document.body.style.opacity = "1";
 }
 
-function applyFadeOutTransition(event) {
+function applyFadeOutTransition(event, callback) {
     event.preventDefault();
     document.body.style.opacity = "0";
     
     setTimeout(() => {
-        if (event.target && event.target.tagName === "A") {
-            window.location.href = event.target.href;
-        } else {
-            window.location.reload();
-        }
-    }, 1000);  
+        callback();
+    }, 1000);
 }
 
-document.addEventListener('DOMContentLoaded', applyFadeInTransition);
-document.addEventListener('click', function(event) {
-    if (event.target.tagName === "A") {
-        applyFadeOutTransition(event);
-    }
-});
